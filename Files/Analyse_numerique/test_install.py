ed37d13a52f93c837be0081e819c4aedfad31f82
Florian Dussap <109675436+fdussap@users.noreply.github.com>
2026-08-25 13:35:35 +0200

    maj test_install python

--- Files/Analyse_numerique/test_install.py
+++ Files/Analyse_numerique/test_install.py
@@ -1,7 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib, scipy
 
 print("NumPy version :", np.__version__)
+print("Matplotlib version :", matplotlib.__version__)
+print("SciPy version :", scipy.__version__)
 
 x = np.linspace(0, 2*np.pi, 100)
 plt.plot(x, np.sin(x))
